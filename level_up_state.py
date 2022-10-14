f39b9ecf9249fafc8db902951ef7f714fdc606f1
Eomjangheon <mpmpm0610@gmail.com>
2022-10-14 19:41:40 +0900

    level up state 추가
    수업을 토대로 메인state가
    level up state에서도 보이게 변경

--- level_up_state.py
+++ level_up_state.py
@@ -8,6 +8,8 @@ import main_state
 from level_up_state_ui import*
 import game_world
 name = "levelUpState"
+#레벨업시 잠시 들어오는 state이다.
+#여기서 스킬 레벨을 올릴수있다.
 def enter():
     global level_up_state_ui
     level_up_state_ui=Level_up_state_ui()
@@ -28,8 +30,13 @@ def handle_events():
         if event.type==SDL_QUIT:
             game_framework.quit()
         elif event.type==SDL_KEYDOWN:
+            #---------------------------------------
+            #escape는 수정 예정이다. 지금은 개발자용 키
+            #---------------------------------------
             if event.key==SDLK_ESCAPE:
                 game_framework.pop_state()
+            
+            #스킬 선택 버튼이다. 스킬 개수에 맞게 이동가능하다.
             elif event.key==SDLK_DOWN and level_up_state_ui.selectNum<len(level_up_state_ui.getSkill)-1:
                 level_up_state_ui.selectNum+=1
             elif event.key==SDLK_UP and level_up_state_ui.selectNum>0:
@@ -44,21 +51,18 @@ def update():
     pass
 
 def draw():
-    global level_up_state_ui
     clear_canvas()
+    global level_up_state_ui
     if(len(level_up_state_ui.getSkill)>0):
         main_state.main_state_ui.draw()
     else:
         game_framework.pop_state()
         return
-    for i in range(9):
-        main_state.backgrounds[i].draw()
-    main_state.player.draw()
+    main_state.drawWorld()
     level_up_state_ui.draw()
     update_canvas()
     
     delay(0.02)
-
 
 def pause(): pass
 def resume(): pass
