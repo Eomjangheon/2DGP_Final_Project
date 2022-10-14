a8644f833cc279f01585bfd5450390330385716b
Eomjangheon <mpmpm0610@gmail.com>
2022-10-14 19:02:20 +0900

    버그수정(레벨업 UI)
    레벨업 UI에서 빈칸 선택 불가능,
    얻을 스킬이 없다면 바로pop

--- level_up_state.py
+++ level_up_state.py
@@ -30,7 +30,7 @@ def handle_events():
         elif event.type==SDL_KEYDOWN:
             if event.key==SDLK_ESCAPE:
                 game_framework.pop_state()
-            elif event.key==SDLK_DOWN and level_up_state_ui.selectNum<2:
+            elif event.key==SDLK_DOWN and level_up_state_ui.selectNum<len(level_up_state_ui.getSkill)-1:
                 level_up_state_ui.selectNum+=1
             elif event.key==SDLK_UP and level_up_state_ui.selectNum>0:
                 level_up_state_ui.selectNum-=1
@@ -46,12 +46,17 @@ def update():
 def draw():
     global level_up_state_ui
     clear_canvas()
+    if(len(level_up_state_ui.getSkill)>0):
+        main_state.main_state_ui.draw()
+    else:
+        game_framework.pop_state()
+        return
     for i in range(9):
         main_state.backgrounds[i].draw()
     main_state.player.draw()
-    main_state.main_state_ui.draw()
     level_up_state_ui.draw()
     update_canvas()
+    
     delay(0.02)
 
 
