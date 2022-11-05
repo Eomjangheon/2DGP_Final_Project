1c7ebdbc1bf2a5ce78e2087940a3f3d5c7916c7f
Eomjangheon <mpmpm0610@gmail.com>
2022-11-06 02:11:59 +0900

    pause state 완성

--- codeFile/stateCode/pause_state.py
+++ codeFile/stateCode/pause_state.py
@@ -1,19 +1,22 @@
 from os import remove
 from pico2d import *
 import game_framework
-import random
 from codeFile.stateCode.main_state_ui import*
 import codeFile.stateCode.main_state as main_state
 from codeFile.stateCode.level_up_state_ui import*
-import game_world
-import value
+
+
 name = "pause_state"
+image=None
 #레벨업시 잠시 들어오는 state이다.
 #여기서 스킬 레벨을 올릴수있다.
 def enter():
-    pass
+    global image
+    image=load_image('res/ui/pause.png')
 
 def exit():
+    global image
+    del image
     pass
 
 
@@ -34,8 +37,10 @@ def update():
     pass
 
 def draw():
+    global image
     clear_canvas()
     main_state.drawWorld()
+    image.draw(640,400,300,300)
     update_canvas()
     
     delay(0.02)
