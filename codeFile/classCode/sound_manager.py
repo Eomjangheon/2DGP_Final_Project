5cb90a48ba7336170f2d564b58d8e5bfad710ddd
Eomjangheon <mpmpm0610@gmail.com>
2022-11-06 03:44:19 +0900

    사운드 수정
    레벨업 사운드가 묻히는 현상 수정

--- codeFile/classCode/sound_manager.py
+++ codeFile/classCode/sound_manager.py
@@ -2,11 +2,13 @@ from pico2d import*
 import value
 #main_state의 UI객체
 class Sound_Manager:
-    soundCheck=[False for i in range(4)]
+    soundCheck=[False for i in range(5)]
     eatGemSound=None
     whip_sound=None
     monster_hit_sound=None
     axe_sound=None
+    lvup_sound=None
+    sound_stack=0
     def __init__(self):
         if Sound_Manager.eatGemSound==None:
             Sound_Manager.eatGemSound=load_wav('res/sound/get_gem_sound.wav')
@@ -20,24 +22,34 @@ class Sound_Manager:
         if Sound_Manager.monster_hit_sound==None:
             Sound_Manager.monster_hit_sound=load_wav('res/sound/enemy_hit_sound.wav')
             Sound_Manager.monster_hit_sound.set_volume(value.volume)
+        if Sound_Manager.lvup_sound==None:
+            Sound_Manager.lvup_sound=load_wav('res/sound/level_up_sound.wav')
+            Sound_Manager.lvup_sound.set_volume(value.volume)
     def gem_eat_sound(self):
-        if not self.soundCheck[0]:
+        if self.soundCheck[0]==False:
             self.eatGemSound.play()
             self.soundCheck[0]=True
     def Whip_sound(self):
-        if not self.soundCheck[1]:
+        if self.soundCheck[1]==False:
             self.whip_sound.play()
             self.soundCheck[1]=True
     def Axe_sound(self):
-        if not self.soundCheck[2]:
+        if self.soundCheck[2]==False:
             self.axe_sound.play()
             self.soundCheck[2]=True
     def Monster_hit_sound(self):
-        if not self.soundCheck[3]:
+        if self.soundCheck[3]==False:
             self.monster_hit_sound.play()
             self.soundCheck[3]=True
+    def Lvup_sound(self):
+        if self.soundCheck[4]==False:
+            self.lvup_sound.play()
+            self.soundCheck[4]=True
     def self_update(self):
-        for i in range(4):
-            self.soundCheck[i]=False
+        self.sound_stack+=1
+        self.sound_stack%=5
+        if self.sound_stack==0:
+            for i in range(5):
+                self.soundCheck[i]=False
 
 
