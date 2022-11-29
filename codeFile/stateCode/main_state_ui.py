8adcd173f585f58a762ae5f0e09e9b03040ec9a7
Eomjangheon <mpmpm0610@gmail.com>
2022-11-30 01:06:36 +0900

    main state 밸런스 수정

--- codeFile/stateCode/main_state_ui.py
+++ codeFile/stateCode/main_state_ui.py
@@ -17,6 +17,7 @@ class Main_state_ui:
     total_miniute=0
     is_spawn_hoodie=False
     mon_time=0
+    level=0
     
     def __init__(self):
         if Main_state_ui.level_font==None:
@@ -41,16 +42,26 @@ class Main_state_ui:
         self.total_time+=game_framework.frame_time
         self.total_second=self.total_time%60
         self.total_miniute=self.total_time//60
-        if(int(self.total_second)==30 and self.is_spawn_hoodie==False):
+        self.level=self.total_time//20
+        if(int(self.total_time)==180 and self.is_spawn_hoodie==False):
             Main_state_ui.is_spawn_hoodie=True
             self.spawn_hoodie()
-        if(len(game_world.objects[3])<300 and self.mon_time>0.15 and self.is_spawn_hoodie==False):
-            mon=Bat()
-            game_world.add_object(mon,3)
-            mon=Armor()
-            game_world.add_object(mon,3)
-            mon=Buer()
-            game_world.add_object(mon,3)
+        if(len(game_world.objects[3])<300 and self.mon_time>0.5 and self.is_spawn_hoodie==False):
+            if(self.level>=0 and self.level<5):
+                mon=Bat()
+                game_world.add_object(mon,3)
+            if(self.level>=2 and self.level<7):
+                mon=Armor()
+                game_world.add_object(mon,3)
+            if(self.level>=5):
+                mon=Armor()
+                game_world.add_object(mon,3)
+            if(self.level>=4 and self.level<10):    
+                mon=Buer()
+                game_world.add_object(mon,3)
+            if(self.level>=6):    
+                mon=Buer()
+                game_world.add_object(mon,3)
             self.mon_time=0
     def spawn_hoodie(self):
         for mon in game_world.objects[3]:
